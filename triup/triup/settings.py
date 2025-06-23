6b9326c0fe862e61e73f5506a3e6e1e957d7674f
Breno Prado Salgado Resende <81430600+brenopsr@users.noreply.github.com>
2025-06-23 09:31:12 -0300

    Fix Django app configuration and template rendering

--- triup/triup/settings.py
+++ triup/triup/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'triup.website',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'triup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'website', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'triup', 'website', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
